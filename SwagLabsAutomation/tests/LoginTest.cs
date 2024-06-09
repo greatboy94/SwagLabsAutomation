5663d6537989ae5e5c864569093c57c543af36eb
Buyukbek <greatboy_94@mail.ru>
2024-06-09 16:27:40 +0500

    Allure configurated

--- SwagLabsAutomation/tests/LoginTest.cs
+++ SwagLabsAutomation/tests/LoginTest.cs
@@ -1,13 +1,17 @@
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
 using SwagLabsAutomation.pageObjects;
 
 namespace SwagLabsAutomation.tests;
 
+[AllureNUnit]
 public class LoginTest : Base
 {
 
     [Test]
+    [AllureDescription("Check for valid username and password login")]
     [TestCaseSource(typeof(JsonReader), nameof(JsonReader.GetValidLoginTestData))]
-    public void ValidLogin(string username, string password)
+    public void TestValidLogin(string username, string password)
     {
         LoginPage loginPage = new LoginPage(GetDriver());
         loginPage.LoginWithCredentials(username, password);
